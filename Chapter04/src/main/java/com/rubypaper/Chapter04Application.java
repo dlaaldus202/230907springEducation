44d1f995f26dbb9cd68f4108993c193327abc4fe
dlaaldus202 <dlaaldus202@naver.com>
2023-09-07 08:38:05 +0900

    [test1]

--- Chapter04/src/main/java/com/rubypaper/Chapter04Application.java
+++ Chapter04/src/main/java/com/rubypaper/Chapter04Application.java
@@ -9,6 +9,7 @@ public class Chapter04Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter04Application.class, args);
 		System.out.println("Chapter04Application");
+		System.out.println("Chapter04Application");
 	}
 
 }
